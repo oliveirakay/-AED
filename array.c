f2541697aebb2111210ebe7289a6a7c8979a5730
Kayque Oliveira <58827916+oliveirakay@users.noreply.github.com>
2024-02-16 22:40:28 +0000

    criando interface de string

--- array.c
+++ array.c
@@ -6,3 +6,12 @@ Array struct {
     int size;
 
 };
+
+Array * array_create(int size){
+    Array array =
+};
+void array_destroy(Array*);
+int array_get(Array*);
+void array_set(Array* array, int pos);
+int array_size(Array*);
+void array_print(Array*);
